05c69cfd40aebc4b7222b2d53193620a29660230
Nick Medovich <nmedovich@gmail.com>
2022-02-03 14:17:18 -0600

    updating cli

--- cli.py
+++ cli.py
@@ -5,8 +5,8 @@ def load_csv():
     print("Please enter the name of a file to upload to the database")
     f_in = input("> ")
     files = {"file": open(f_in, "rb")}
-    url = "http://0.0.0.0/load_csv"
-    requests.post(url,files=files)
+    url = "http://localhost:5000/load_csv"
+    requests.post(url, files=files)
 
 def list_players():
     pass
