714585012e02f105105b5bd5592d7a66055c48c3
Nick Medovich <nmedovich@gmail.com>
2022-02-02 22:50:35 -0600

    initial commit to main, should have checked out dev first, but it's late

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, url_for, redirect, make_response
+from flask import request
+from my_types.types import Team, Player, Game
+import boto3
+import csv
+import os
+
+
+app = Flask(__name__)
+ALLOWED_EXTENSIONS = ["csv"]
+UPLOAD_FOLDER = "./temp"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def sanitize(string_in):
+	return string_in.replace("\"", "").strip()
+
+def allowed_file(filename):
+    return "." in filename and \
+    	filename.split(".")[:-1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+	return render_template("index.html", title="Interview Questions",
+		header="Queries")
+
+@app.route("/load_csv", methods=["GET", "POST"])
+def load_csv():
+	bucket = "nmedovich-interview"
+	file_name = ""
+	s3_upload = True
+	# Load flat file from S3
+	if request.method == "GET":
+		file_name = "mlb_players.csv"
+	else:
+		# Allow user to upload file, will not check for sanitation
+		if "upload" in request.args:
+			# Empty file sent
+			if "file" not in request.files:
+	            res = make_response('No "file" body detected', 200)
+				res.mimetype = "text/plain"
+				return res
+			file = request.files["file"]
+			# No filename, so no file sent
+			if not file.file_name:
+				res = make_response('No file selected', 200)
+				res.mimetype = "text/plain"
+				return res
+			# Valid filetype filename exists
+			if file and allowed_file(file.filename):
+				s3_upload = False
+				file_name = file.filename
+				file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+		else:
+			# Allow user to specify a file in the interview bucket
+			if "file_name" in request.args:
+				file_name = request.args["file_name"]
+	lines = None
+	# Use S3 File
+	if s3_upload:
+		s3 = boto3.client("s3")
+		s3_obj = s3.get_object(Bucket=bucket, Key=file_name)["Body"]
+		lines = s3_obj.read().decode('utf-8').splitlines(True)
+	# Use Flat file or Uploaded file
+	else:
+		lines = open(file_name, "r"):
+	# Ignoring Headers
+	first_line = True
+	teams = []
+	players = []
+	for row in csv.reader(lines):
+		if not first_line:
+			try:
+				# Prep Team Object
+				team_name = sanitize(row[1])
+				team = Team(team_name)
+				teams.append(team)
+				# Prep Player Object
+				name = sanitize(row[0])
+				height = row[3]
+				weight = row[4]
+				age = row[5]
+				position = santitize(row[2])
+				team_name = sanitize(row[1])
+				player = Player(
+					name, height, weight, age, position, team_name
+				)
+				players.append(player)
+			except IndexError:
+				break
+		else:
+			first_line = False
+	# Set up database connector to pass to helper functions
+	host = "localhost"
+	user = "root"
+	database = "mlb_players_2"
+	password = ""
+	with open("secret.txt", "r") as f_in:
+		password = f_in.read()
+	db_conn = mysql.connector.connect(
+		host = host,
+		user = user,
+		password = password,
+		database = database
+	)
+	# Add teams to table if they don't exist, ignore if they do
+	Team.load_teams(teams, db_conn)
+	# Retrieve teams and team IDs for Player objects
+	teams_dict = Team.get_teams(db_conn)
+	for player in players:
+		player.set_team_id(teams_dict[player.team_name])
+	# Add players to table (ignoring existant records for testing)
+	Player.load_players(players, db_conn)
+	# We're finally done with loading the databases
+	res = make_response("Successfully loaded", 200)
+	res.mimetype = "text/plain"
+	return res
+
