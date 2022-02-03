581bf709c7764e9543767c2355c60603d94cfd7b
Nick Medovich <nmedovich@gmail.com>
2022-02-03 10:15:41 -0600

    updating tests

--- test_mysql.py
+++ test_mysql.py
@@ -7,8 +7,9 @@ def test():
     database = "mlb_players"
     password = ""
     with open("secret.txt", "r") as f_in:
-        password = f_in.read()
+        password = f_in.read().split("\n")[0]
     print(password)
+
     db_conn = mysql.connector.connect(
         host = host,
         user = user,
